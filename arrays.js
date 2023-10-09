09afecd3337493b105c4a53d26176514cb96ca47
Arun1318 <122969475+Arun1318@users.noreply.github.com>
2023-10-09 22:38:30 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,24 @@
+// // //sorting of numbers in ascending in an array
+// var arr=[2,8,1,5,3,88,99,44,22]
+// // //console.log(arr.sort())   //1, 2, 22,  3, 44,5, 8, 88, 99
+// console.log(arr.reverse()) // 22, 44, 99, 88, 3,5,  1,  8,  2
+
+// // console.log(arr.sort((a,b)=>{return a-b;})) // 1,  2,  3,  5, 8,22, 44, 88, 99
+// // //highest number
+// // console.log(arr[arr.length-1])
+// // //smallest number
+// // console.log(arr[0])
+// // //sorting decending
+
+// // console.log(arr.sort((a,b)=>{return b-a})) //99, 88, 44, 22, 8,5,  3,  2,  1
+
+// var arr1=["thor","captain","ironman","superman","batman"]
+// console.log(arr1.sort()) //[ 'batman', 'captain', 'ironman', 'superman', 'thor' ]
+// console.log(arr1.reverse()) //[ 'thor', 'superman', 'ironman', 'captain', 'batman' ]
+
+// var arr2=['a','c','e','w','g']  //[ 'a', 'c', 'e', 'g', 'w' ]
+// console.log(arr2.sort())
+
+var arr3=[3,1,44,98,34]
+console.log(Math.max(...arr3)) //98
+console.log(Math.min(...arr3))//1
